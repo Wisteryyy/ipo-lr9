7b16807b39a2e53fe49dfea674caee3de33a0ef8
Sofia Skakun <sofiaskakun30@gmail.com>
2024-11-24 20:20:50 +0300

    isCorrectRect.py

--- collision/isCorrectRect.py
+++ collision/isCorrectRect.py
@@ -1,6 +1,12 @@
 def isCorrectRect(coords):
+    if len(coords) != 2:
+        return False
+    
     left_bottom, right_up = coords
-
+    
+    if len(left_bottom) != 2 or len(right_up) != 2:
+        return False
+    
     x1, y1 = left_bottom
     x2, y2 = right_up
 
