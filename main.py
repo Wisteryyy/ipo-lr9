a05bd05b14d505f2f64133cce348387b91e62dfc
Sofia <sofiaskakun30@gmail.com>
2024-11-26 20:44:25 +0300

    main.py

--- main.py
+++ main.py
@@ -6,40 +6,94 @@ from collision import isCollisionRect
 from collision import intersectionAreaRect
 from collision import intersectionAreaMultiRect
 
+print("--------Проверяемые функции--------")
+print("------1. isCorrectRect-------------")
+print("------2. isCollisionRect-----------")
+print("------3. intersectionAreaRect------")
+print("------4. intersectionAreaMultiRect-")
 
-print(isCorrectRect.isCorrectRect([(-3.4, 1), (9.2, 10)]))
-print(isCorrectRect.isCorrectRect([(-7, 9), (3, 6)]))
+a = int(input("Введите номер проверяемой функции: "))
 
-try:
-    print(isCollisionRect.isCollisionRect([(-3.4, 1), (9.2, 10)], [(-7.4, 0), (13.2, 12)]))
-    print(isCollisionRect.isCollisionRect([(1, 1), (2, 2)], [(3, 0), (13, 1)]))
-    print(isCollisionRect.isCollisionRect([(1, 1), (2, 2)], [(3, 17), (13, 1)]))
-except RectCorrectError as e:
-    print(e)
+if a == 1:
+    while True:
+        try:
+            x1 = float(input("Введите координату x1: "))
+            y1 = float(input("Введите координату y1: "))
+            x2 = float(input("Введите координату x2: "))
+            y2 = float(input("Введите координату y2: "))
+            print(isCorrectRect([(x1, y1),(x2, y2)]))
+            break
+        except ValueError:
+            print("Введенное Вами число должно быть целое или дробное. Попробуйте еще раз.")
+        
+if a == 2:
+    while True:
+        try:
+            x11 = float(input("Введите координату x11: "))
+            y11 = float(input("Введите координату y11: "))
+            x12 = float(input("Введите координату x12: "))
+            y12 = float(input("Введите координату y12: "))
+            x21 = float(input("Введите координату x21: "))
+            y21 = float(input("Введите координату y21: "))
+            x22 = float(input("Введите координату x22: "))
+            y22 = float(input("Введите координату y22: "))
+            try:
+                print(isCollisionRect([(x11, y11), (x12, y12)], [(x21, y21), (x22, y22)]))
+            except RectCorrectError as e:
+                print(e)
+            break
+        except ValueError:
+            print("Введенное Вами число должно быть целое или дробное. Попробуйте еще раз.")
+            
+if a == 3:
+    while True:
+        try:
+            x11 = float(input("Введите координату x11: "))
+            y11 = float(input("Введите координату y11: "))
+            x12 = float(input("Введите координату x12: "))
+            y12 = float(input("Введите координату y12: "))
+            x21 = float(input("Введите координату x21: "))
+            y21 = float(input("Введите координату y21: "))
+            x22 = float(input("Введите координату x22: "))
+            y22 = float(input("Введите координату y22: "))
+            try:
+                print(intersectionAreaRect([(x11, y11), (x12, y12)], [(x21, y21), (x22, y22)]))
+            except RectCorrectError as e:
+                print(e)
+            break
+        except ValueError:
+            print("Введенное Вами число должно быть целое или дробное. Попробуйте еще раз.")
+            
+if a == 4:
+    while True:
+        try:
+            b = int(input("Введите количество прямоугольников в списке прямоугольников: "))
+            rectangles = []
+            
+            for i in range(b):
+                print(f"Введите координаты для прямоугольника {i + 1}:")
+                while True:
+                    try:
+                        x1 = float(input("Введите x1: "))
+                        y1 = float(input("Введите y1: "))
+                        x2 = float(input("Введите x2: "))
+                        y2 = float(input("Введите y2: "))
+                        
+                        if x1 >= x2 or y1 >= y2:
+                            print("Координаты некорректны: x1 должно быть меньше x2, а y1 должно быть меньше y2.")
+                        else:
+                            rectangles.append([(x1, y1), (x2, y2)])
+                            break
+                    
+                    except ValueError:
+                        print("Введенное Вами число должно быть целое или дробное. Попробуйте еще раз.")
 
-try:
-    print(intersectionAreaRect.intersectionAreaRect([(-3, 1), (9, 10)], [(-7, 0), (13, 12)]))
-    print(intersectionAreaRect.intersectionAreaRect([(1, 1), (2, 2)], [(3, 0), (13, 1)]))
-    print(intersectionAreaRect.intersectionAreaRect([(1, 1), (2, 2)], [(3, 17), (13, 1)]))
-except RectCorrectError as e:
-    print(e)
+            result = intersectionAreaMultiRect(rectangles)
+            print(f"Уникальная площадь пересечения: {result}")
+            break
+            
+        except ValueError:
+            print("Введенное Вами число должно быть целое. Попробуйте еще раз.")
 
-rectangles = [
-    [(2, 2), (5, 4)],
-    [(0, 1), (2, 2)],
-    [(1, 1), (4, 4)],
-    [(2, -1), (3, 3)]
-]
-
-result = intersectionAreaMultiRect(rectangles)
-print(f"Уникальная площадь пересечения: {result}")
-
-incorrect_rectangles = [
-    [(-3, 1), (9, 10)],
-    [(3, 17), (13, 1)]
-]
-
-try:
-    intersectionAreaMultiRect(incorrect_rectangles)
-except RectCorrectError as e:
-    print(e)
+if a < 1 or a > 4:
+    print("Введенное Вами число некорректно. Попробуйте еще раз.")
