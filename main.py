5654ca8d081bfb7efa767d7fa7e875706e7b9c9e
Sofia <sofiaskakun30@gmail.com>
2024-11-26 21:54:32 +0300

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
+# Пример ввода 1. isCorrectRect
+# isCorrectRect([(-3.4, 1),(9.2, 10)]) # Вернет True
+# isCorrectRect([(-7, 9),(3, 6)]) # Вернет False
+
+# Пример ввода 2. isCollisionRect
+# isCollisionRect([(-3.4, 1),(9.2, 10)], [(-7.4, 0),(13.2, 12)]) # Вернет True
+# isCollisionRect([(1, 1),(2, 2)], [(3, 0),(13, 1)]) # Вернет False
+# isCollisionRect([(1, 1),(2, 2)], [(3, 17),(13, 1)]) # Вызовет ошибку
+
+# Пример ввода 3. intersectionAreaRect
+# intersectionAreaRect([(-3, 1), (9, 10)], [(-7, 0), (13, 12)]) # Вернет некоторое положительное число
+# intersectionAreaRect([(1, 1), (2, 2)], [(3, 0), (13, 1)]) # Вернет 0
+# intersectionAreaRect([(1, 1), (2, 2)], [(3, 17), (13, 1)]) # Вызовет ошибку
+
+# Пример ввода 4. intersectionAreaMultiRect
+# rectangles = [ # Корректные прямоугольники, покажет уникальную площадь
+#     [(-3, 1), (9, 10)],
+#     [(-7, 0), (13, 12)],
+#     [(0, 0), (5, 5)],
+#     [(2, 2), (7, 7)]
+# ]
+
+# incorrect_rectangles = [
+#     [(-3, 1), (9, 10)],
+#     [(3, 17), (13, 1)]  # Некорректный прямоугольник, вызовет ошибку
+# ]
 class RectCorrectError(Exception):
     pass
 
-from collision import isCorrectRect
-from collision import isCollisionRect
-from collision import intersectionAreaRect
-from collision import intersectionAreaMultiRect
+from collision.isCorrectRect import isCorrectRect
+from collision.isCollisionRect import isCollisionRect
+from collision.intersectionAreaRect import intersectionAreaRect
+from collision.intersectionAreaMultiRect import intersectionAreaMultiRect
 
 print("--------Проверяемые функции--------")
 print("------1. isCorrectRect-------------")
