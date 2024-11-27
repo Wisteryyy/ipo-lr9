1a74def6d738c1b34e2cb74cd5502c438e215d6c
Sofia <sofiaskakun30@gmail.com>
2024-11-27 21:27:13 +0300

    isCollisionRect.py

--- collision/isCollisionRect.py
+++ collision/isCollisionRect.py
@@ -6,8 +6,8 @@ def isCollisionRect(coords1, coords2):
     (x21, y21), (x22, y22) = coords2
 
     if x11 >= x12 or y11 >= y12:
-        raise RectCorrectError("1й прямоугольник некоректный")
+        raise ValueError("1й прямоугольник некоректный")
     if x21 >= x22 or y21 >= y22:
-        raise RectCorrectError("2й прямоугольник некоректный")
+        raise ValueError("2й прямоугольник некоректный")
 
     return x11 < x22 and x12 > x21 and y11 < y22 and y12 > y21
